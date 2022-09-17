492e17383275d1d70f3c1e5f6d510cbaf1478535
Jeffield <77764638+pchunter97@users.noreply.github.com>
2022-09-17 15:11:32 -0500

    Ejercicio 2
    Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
    
    - Tu nombre (string)
    
    - Tu edad (number)
    
    - ¿Eres desarrollador? (boolean)
    
    - Tu fecha de nacimiento (Date)
    
    - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)

---
+++ Ejercicio2.js
@@ -0,0 +1,20 @@
+//Nombre
+let nombre="Jefferson"
+
+//Edad
+let edad=25
+
+//Eres desarrollador?
+let desarrollador=true
+
+//Fecha de nacimiento
+const fecha_nac = new Date("Januay 8 1997")
+
+//Libro favorito
+const libro_fav={
+    titulo:"Harry Potter",
+    autor:"J.K Rowling",
+    fecha:new Date(1997, 0,8)
+}
+
+const lista = [nombre,edad,desarrollador,fecha_nac,libro_fav]
